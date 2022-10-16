b406390b79e9631b08ab5f1a80f6b5e898838dc4
fija29 <jahangirshaikh318@gmail.com>
2022-10-16 19:49:01 +0530

    first_commit

---
+++ Day 1 programs/problem2.js
@@ -0,0 +1,6 @@
+let name="Fija";
+console.log(name);
+let fatherName="Jahangir";
+console.log(fatherName);
+let motherName="Hasina";
+console.log(motherName);
---
+++ Day 1 programs/problem3.js
@@ -0,0 +1,5 @@
+let name="fija";
+let age=21;
+console.log(name,age);
+console.log(typeof(name),typeof(age));
+
